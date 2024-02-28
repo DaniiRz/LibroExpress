afd9b145a482a732317372370387101fb2ec2e5a
FranciscoGarc <129241621+FranciscoGarc@users.noreply.github.com>
2024-02-28 17:23:07 -0600

    c

--- Querys/LibroExpress.sql
+++ Querys/LibroExpress.sql
@@ -1,5 +1,4 @@
 DROP DATABASE IF EXISTS LibroExpress;
-
 CREATE DATABASE LibroExpress;
 USE LibroExpress;
 
@@ -51,7 +50,6 @@ CREATE TABLE tb_detalles_prestamos (
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
 /*Trigger*/
 DELIMITER //
 CREATE TRIGGER actualizar_estado_libro
@@ -70,7 +68,7 @@ DELIMITER //
 CREATE PROCEDURE AgregarCliente(nombre_cliente VARCHAR(50), email_cliente VARCHAR(100), telefono VARCHAR(10))
 BEGIN
  
-    INSERT INTO tb_clientes(nombre_cliente, email_cliente, telefono) VALUES (nombre_cliente, email_cliente, telefono);
+	INSERT INTO tb_clientes(nombre_cliente, email_cliente, telefono) VALUES (nombre_cliente, email_cliente, telefono);
  
 END //
  
@@ -98,7 +96,6 @@ DELIMITER //
 CREATE PROCEDURE AgregarPrestamo(nombreCliente VARCHAR(50), fecha_inicio DATE, fecha_devolucion DATE, estado ENUM('Activo','Inactivo'))
 BEGIN
     DECLARE cliente_id BINARY(36);
-    -- Encierra la consulta SELECT entre paréntesis
     SET cliente_id = (SELECT id_cliente FROM tb_clientes WHERE nombre_cliente = nombreCliente);
     INSERT INTO tb_prestamos(id_cliente, fecha_inicio, fecha_devolucion, estado) VALUES (cliente_id, fecha_inicio, fecha_devolucion, estado);
 END //
@@ -154,7 +151,6 @@ DELIMITER //
 CREATE PROCEDURE AgregarLibro(titulo_libro VARCHAR(50), anio_publicacion INT, NombreGenero VARCHAR(50), estado ENUM('Disponible','Prestado'))
 BEGIN
 	DECLARE generoLibro_id BINARY(36);
-    -- Encierra la consulta SELECT entre paréntesis
    SET generoLibro_id = (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = NombreGenero);
  
    INSERT INTO tb_libros(titulo_libro, anio_publicacion, id_genero_libro, estado) VALUES (titulo_libro, anio_publicacion, generoLibro_id, estado);
@@ -186,7 +182,6 @@ BEGIN
 	DECLARE cliente_id BINARY(36);
 	DECLARE prestamo_id BINARY(36);
 	DECLARE libros_id BINARY(36);
-    -- Encierra la consulta SELECT entre paréntesis
    SET cliente_id = (SELECT id_cliente FROM tb_clientes WHERE nombre_cliente = nombre_cliente LIMIT 1);
    SET prestamo_id = (SELECT id_prestamo FROM tb_prestamos WHERE id_cliente = cliente_id LIMIT 1);
    SET libros_id = (SELECT id_libro FROM tb_libros WHERE titulo_libro = tituloLibro LIMIT 1);
