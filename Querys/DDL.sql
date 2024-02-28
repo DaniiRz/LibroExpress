d949241330d98a12c3e3ce59deabe82ba04749f3
FranciscoGarc <129241621+FranciscoGarc@users.noreply.github.com>
2024-02-28 17:22:03 -0600

    c

--- Querys/DDL.sql
+++ Querys/DDL.sql
@@ -1,3 +1,52 @@
+/*Tablas*/
+CREATE TABLE tb_clientes (
+  id_cliente VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+  nombre_cliente VARCHAR(50) NOT NULL,
+  email_cliente VARCHAR(100) NOT NULL,
+  telefono VARCHAR(10) NOT NULL
+);
+
+
+CREATE TABLE tb_prestamos (
+  id_prestamo VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+  id_cliente VARCHAR(36) NOT NULL,
+  fecha_inicio DATE NOT NULL,
+  fecha_devolucion DATE NOT NULL,
+  estado ENUM('Activo', 'Inactivo') NOT NULL,
+  CONSTRAINT fk_prestamo_cliente
+    FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE tb_generos_libros (
+  id_genero_libro VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+  nombre_genero_libro VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tb_libros (
+  id_libro VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+  titulo_libro VARCHAR(50) NOT NULL,
+  anio_publicacion INT NOT NULL,
+  id_genero_libro VARCHAR(36) NOT NULL,
+  estado ENUM('Disponible', 'Prestado') NOT NULL,
+  CONSTRAINT fk_libro_genero
+    FOREIGN KEY (id_genero_libro) REFERENCES tb_generos_libros(id_genero_libro)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE tb_detalles_prestamos (
+  id_detalle_prestamo VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+  id_prestamo VARCHAR(36) NOT NULL,
+  id_libro VARCHAR(36) NOT NULL,
+  CONSTRAINT fk_detalle_prestamo
+    FOREIGN KEY (id_prestamo) REFERENCES tb_prestamos(id_prestamo)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_detalle_libro
+    FOREIGN KEY (id_libro) REFERENCES tb_libros(id_libro)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 /*Trigger*/
 DELIMITER //
 CREATE TRIGGER actualizar_estado_libro
